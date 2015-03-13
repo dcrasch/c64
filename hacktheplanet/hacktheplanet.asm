1fe05f31c619527f0fd94abf80beed046db6296a
David Rasch <david.rasch@buyitdirect.com>
2015-03-13 16:17:05 +0100

    add gpl

--- hacktheplanet/hacktheplanet.asm
+++ hacktheplanet/hacktheplanet.asm
@@ -1,9 +1,12 @@
+	;; (C) 2015 by David Rasch (david@hacklab.nl)
+	;; Some rights reserved. See COPYING.
+	
 	.processor 6502 
 	.org $801
 	.hex 0c08 0000 9e32 3036 3100 0000 ; basic start header
 
 	jsr $e544	;clear the screen
-	ldx #$00;
+	ldx #$00
 loop	lda message,x
 	jsr $ffd2 ;print the character
 	inx
