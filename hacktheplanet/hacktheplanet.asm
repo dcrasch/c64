f08d19e34956816689d5c09373bbdde5f7244390
David Rasch <dcrasch@gmail.com>
2015-03-13 10:05:29 +0100

    Update hacktheplanet.asm
    change string length

--- hacktheplanet/hacktheplanet.asm
+++ hacktheplanet/hacktheplanet.asm
@@ -1,13 +1,13 @@
 	.processor 6502 
 	.org $801
-	.hex 0c08 0000 9e32 3036 3100 0000
+	.hex 0c08 0000 9e32 3036 3100 0000 ; basic start header
 
 	jsr $e544	;clear the screen
 	ldx #$00;
 loop	lda message,x
 	jsr $ffd2 ;print the character
 	inx
-	cpx #$12
+	cpx #$10
 	bne loop
 	rts ;return from subroutine
 
